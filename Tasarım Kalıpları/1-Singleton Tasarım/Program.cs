5bbfc50f255516d4f061db3e306f510e3477b788
mikayil aktemur <mikayil34@gmail.com>
2021-11-27 17:51:02 +0300

    tasarım desenleri

---
+++ Tasarım Kalıpları/1-Singleton Tasarım/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Singleton_Tasarım
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var costumManeger = CostumManeger.CreateAsSingleton();
+            costumManeger.Save();
+
+        }
+
+    }
+
+    class CostumManeger
+    {
+        static CostumManeger _costumManeger;
+        private CostumManeger()
+        {
+
+        }
+        public static CostumManeger CreateAsSingleton()
+        {
+            return _costumManeger ?? (_costumManeger = new CostumManeger());
+
+        }
+        public  void Save()
+        { 
+            Console.WriteLine("Saved");
+            Console.ReadLine();
+        }
+
+    }
+
+}
